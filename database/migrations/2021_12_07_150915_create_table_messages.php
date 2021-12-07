c9c3f2232d41d365d13ad5238f606bd0ae81026b
trandinhhieu20152 <trandinhhieu20152@gmail.com>
2021-12-07 22:39:59 +0700

    hi

---
+++ database/migrations/2021_12_07_150915_create_table_messages.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableMessages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->longText('message')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('receiver');
+            $table->foreign('receiver')->references('id')->on('users');
+            $table->boolean('is_seen')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
